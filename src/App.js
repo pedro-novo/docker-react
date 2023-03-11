8629d800125b73c12587af4587473e85780be05c
pedro-novo <pedro.novo.93@gmail.com>
2023-03-11 07:51:38 +0000

    feature test

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Just Changed this to check Github Actions Workflow!
         </a>
       </header>
     </div>
